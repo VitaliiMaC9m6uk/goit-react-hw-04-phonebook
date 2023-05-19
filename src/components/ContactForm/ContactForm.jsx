902f369873e06c63a006578466ac04596184ca6b
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-05-19 11:40:45 +0200

    finish

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,47 +1,52 @@
-import { Component } from "react";
+import {useState } from "react";
 import { Form } from "./ContactForm.styled";
+const ContactForm = ({submit}) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+  const hendleChange = ({ target: { value, name } }) => {     
+    switch (name) {
+      case 'name':
+        setName(value)
+        break;
+      case 'number':
+        setNumber(value)
+        break;
+      default:
+        return;
+    }
   };
-  hendleChange = ({ target: { value, name } }) => {
-    
-    this.setState({
-      [name]: value,
-    });
-  };
-  hendleSubmit = e => {
+
+  const hendleSubmit = e => {
     e.preventDefault();
-    this.props.submit(this.state)
+    submit({ name, number });
   }
-  render() {
-    return (
-      <Form onSubmit={this.hendleSubmit}>
-        <h2>Name</h2>
-        <input
-          onChange={this.hendleChange}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          value={this.name}
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        ></input>
-        <h2>Number</h2>
-        <input
-          type="tel"
-          value={this.number}
-          onChange={this.hendleChange}
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        ></input>
-        <button type="submit">Add contact</button>
-      </Form>
-    );
-  }
+  
+  return (
+    <Form onSubmit={hendleSubmit}>
+      <h2>Name</h2>
+      <input
+        onChange={hendleChange}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        value={name}
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      ></input>
+      <h2>Number</h2>
+      <input
+        type="tel"
+        value={number}
+        onChange={hendleChange}
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      ></input>
+      <button type="submit">Add contact</button>
+    </Form>
+  );
 }
+
 export default ContactForm;
--- src/components/ListContacts/ListContacts.jsx
+++ src/components/ListContacts/ListContacts.jsx
@@ -1,29 +1,18 @@
-import { Component } from "react";
 import { Button, Contact, ContactsList } from "./ListContacts.styled";
 
-class ListContacts extends Component {   
-
-  componentDidUpdate(prevProps, prevState) {  
-    if (prevProps.list !== this.props.list) {
-      localStorage.setItem('contacts',JSON.stringify(this.props.list))
-    }
-}
-  
-render(){  
-    return (
-      <ContactsList>
-        {this.props.list.map(contact => (          
-            <Contact key = {contact.id}>
-              {contact.name} : {contact.number}
-            <Button
-              id={contact.id}
-              onClick={this.props.deleteName}
-            >Delete</Button>
-            </Contact>
-          
-        ))}
-      </ContactsList>
-    );
-  };
+const ListContacts = ({list,deleteName}) => {
+    
+  return (
+    <ContactsList>
+      {list.map(contact => (
+        <Contact key={contact.id}>
+          {contact.name} : {contact.number}
+          <Button id={contact.id} onClick={deleteName}>
+            Delete
+          </Button>
+        </Contact>
+      ))}
+    </ContactsList>
+  );
 }
 export default ListContacts;
